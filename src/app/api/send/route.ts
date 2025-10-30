dbbcfd46266acdf17e2ab4caa5980208f502f39f
Steve McKinnon <32041213+stevenmckinnon@users.noreply.github.com>
2025-10-30 22:47:20 +0000

    Add 'botid' package for bot detection in API; implement bot check in send route to restrict access

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,4 +1,5 @@
 import { Resend } from "resend";
+import { checkBotId } from "botid/server";
 
 import EmailTemplate from "@/components/emails/contact";
 import type { ReactElement } from "react";
@@ -7,13 +8,24 @@ import { DATA } from "@/data/resume";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
+  const { isBot } = await checkBotId();
+
+  if (isBot) {
+    return new Response("Access Denied", { status: 403 });
+  }
+
   try {
     const { name, email, message, subject } = await request.json();
     const { data, error } = await resend.emails.send({
       from: `${name} <noreply@stevenmckinnon.co.uk>`,
       to: [DATA.contact.email],
       subject: subject,
-      react: EmailTemplate({ message, name, email, subject }) as ReactElement<any>,
+      react: EmailTemplate({
+        message,
+        name,
+        email,
+        subject,
+      }) as ReactElement<any>,
     });
 
     if (error) {
